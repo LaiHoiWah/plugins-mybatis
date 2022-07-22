b9de7246f44c14288a49b1d96a0b41a1d343930d
lihaihua <133313829@qq.com>
2022-07-22 19:14:30 +0800

    field annotation

---
+++ src/main/java/com/meowu/plugins/mybatis/mysql/stereotype/annotation/Field.java
@@ -0,0 +1,11 @@
+package com.meowu.plugins.mybatis.mysql.stereotype.annotation;
+
+import java.lang.annotation.*;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.ANNOTATION_TYPE})
+public @interface Field{
+
+    String value() default "";
+}
